16efe24554976844f205a1328e0f1a7f26384bdc
Rishikesh Mandal <me.rishi29@gmail.com>
2020-04-01 18:33:54 -0400

    Added model class Employee, EmployeeControllerclass and data.sql

---
+++ EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/ems/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.ems.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ems.model.Employee;
+import com.ems.repository.EmployeeRepository;
+
+@RestController
+@RequestMapping("api/v1")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+@GetMapping("/employees")	
+public List<Employee> findAllEmployees(){
+	return employeeRepository.findAll();
+		
+}
+	
+	
+
+}
---
+++ EmployeeManagementSystem/EmployeeManagementSystem/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into employees(id, first_name, last_name, email_id) values(1,"Rishi", "Mandal", "R@g.com");
+insert into employees(id, first_name, last_name, email_id) values(2,"Deepali","Labh", "D@g.com");
+insert into employees(id, first_name, last_name, email_id) values(3, "Rohit", "Karn","Ro@g.com");
