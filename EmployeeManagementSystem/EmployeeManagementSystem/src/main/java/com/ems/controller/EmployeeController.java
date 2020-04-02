c996b1c220eaf959d35ef5c9d28076c6b7a2dede
Rishikesh Mandal <me.rishi29@gmail.com>
2020-04-01 21:16:25 -0400

    Added CRUD operation in controller Exception handler

--- EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/ems/controller/EmployeeController.java
+++ EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/ems/controller/EmployeeController.java
@@ -1,28 +1,83 @@
 package com.ems.controller;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.ems.exception.ResourceNotFoundException;
 import com.ems.model.Employee;
 import com.ems.repository.EmployeeRepository;
 
 @RestController
 @RequestMapping("api/v1")
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
+
+	@GetMapping("/employees")
+	public List<Employee> findAllEmployees() {
+		return employeeRepository.findAll();
+
+	}
+
+	@GetMapping("employee/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId)
+			throws ResourceNotFoundException {
+
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for the Id " + employeeId));
+		return ResponseEntity.ok().body(employee);
+	}
+
+	@PostMapping("/employees")
+	public ResponseEntity<Object> createEmployee(@Valid @RequestBody Employee employee) {
+
+		Employee savedEmployee= employeeRepository.save(employee);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedEmployee.getId()).toUri();
+		return ResponseEntity.created(location).build();
+	}
+
+	@PutMapping("employee/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable(value = "id") Long employeeId,
+			@Valid @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+
+		Employee employee = employeeRepository.findById(employeeId)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this Id " + employeeId));
+
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+
+		final Employee updateEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok().body(updateEmployee);
+	}
 	
-@GetMapping("/employees")	
-public List<Employee> findAllEmployees(){
-	return employeeRepository.findAll();
+	@DeleteMapping("/employee/{id}")
+	public Map<String, Boolean> deleteEmployee(@PathVariable (value= "id") Long employeeId) throws ResourceNotFoundException{
+		 
+		Employee employee = employeeRepository.findById(employeeId).orElseThrow(()->new ResourceNotFoundException("Employee not found with this Id "+ employeeId));
+		employeeRepository.delete(employee);
 		
-}
-	
-	
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+		
+	}
 
 }
--- EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/ems/model/Employee.java
+++ EmployeeManagementSystem/EmployeeManagementSystem/src/main/java/com/ems/model/Employee.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 public class Employee {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@Column(name = "first_name", nullable = false)
 	private String firstName;
